59afa190c9896e9e97c9a5221b3a824c59b311a6
Shishir19999 <shishirsharma199@gmail.com>
2024-05-16 18:13:15 +0545

    update

---
+++ Backend/models/ChatMessage.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+ 
+const chatMessageSchema = new mongoose.Schema({
+    user: { type: String, required: true },
+    message: { type: String, required: true },
+    timestamp: { type: Date, default: Date.now },
+});
+ 
+const ChatMessage = mongoose.model('ChatMessage', chatMessageSchema);
+ 
+export default ChatMessage;
--- Backend/server.js
+++ Backend/server.js
@@ -1,24 +1,25 @@
-// server.js
- 
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
- 
-const ChatMessage = require("./models/ChatMessage");
- 
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import ChatMessage from './models/ChatMessage.js';
+
+dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
- 
+
 // Middleware
 app.use(cors());
 app.use(express.json());
- 
+
 // MongoDB Connection
-mongoose.connect("Your MongoDB connection string", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
 });
- 
+
 // Routes
 app.get("/messages", async (req, res) => {
     try {
@@ -29,31 +30,29 @@ app.get("/messages", async (req, res) => {
         res.status(500).json({ error: "Internal Server Error" });
     }
 });
- 
+
 app.post("/messages", async (req, res) => {
     try {
         const { user, message } = req.body;
- 
+
         if (!user || !message) {
-            return res
-                .status(400)
-                .json({ error: "User and message are required" });
+            return res.status(400).json({ error: "User and message are required" });
         }
- 
+
         const chatMessage = new ChatMessage({
             user,
             message,
         });
- 
+
         await chatMessage.save();
- 
+
         res.status(201).json(chatMessage);
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: "Internal Server Error" });
     }
 });
- 
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
